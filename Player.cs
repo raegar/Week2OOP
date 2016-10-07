257cba7b9dfffe679f341b87b9250e3978c819c8
Jamie Myland <jamie.myland@gmail.com>
2016-10-07 11:56:23 +0100

    Added player movement

--- Player.cs
+++ Player.cs
@@ -8,6 +8,8 @@ namespace ConsoleApplication
         int xPosition;
         int yPosition;
         char sprite;
+        int health;
+        int speed;
 
         public int XPosition
         {
@@ -25,12 +27,47 @@ namespace ConsoleApplication
             sprite = '&';
             xPosition = 3;
             yPosition = 3;
+            speed = 1;
         }
 
         public void Draw()
         {
+            Console.CursorLeft = xPosition;
+            Console.CursorTop = yPosition;
             Console.Write(sprite);
         }
+
+        public void Move(string direction)
+        {
+            switch(direction)
+            {
+                case "up" :
+                    yPosition -= speed;
+                break;
+
+                case "down" :
+                    yPosition += speed;
+                break;
+                
+                case "left" :
+                    xPosition -= speed;
+                break;
+                
+                case "right" :
+                    xPosition += speed;
+                break;
+                
+                default:
+                break;
+
+
+            }
+
+
+
+        }
+
+
 
     }
 
--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApplication
         {
 
             Player player1 = new Player();
+            Player player2 = new Player();
 
 
             Console.WriteLine("Hello Jamie!");
@@ -15,7 +16,34 @@ namespace ConsoleApplication
 
             Console.Clear();
             player1.Draw();
+            player2.Draw();
 
+            while(true)
+            {
+                string direction = "";
+                ConsoleKeyInfo keyPress = Console.ReadKey(true);
+                if(keyPress.Key == ConsoleKey.UpArrow)
+                {
+                    direction = "up";
+                }
+                if(keyPress.Key == ConsoleKey.DownArrow)
+                {
+                    direction = "down";
+                }
+                if(keyPress.Key == ConsoleKey.LeftArrow)
+                {
+                    direction = "left";
+                }
+                if(keyPress.Key == ConsoleKey.RightArrow)
+                {
+                    direction = "right";
+                }
+
+                player1.Move(direction);
+                Console.Clear();
+                player1.Draw();
+                player2.Draw();
+            }
             Console.ReadKey(true);
         }
     }

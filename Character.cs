b058dbb29e5cdd2df1f7738dffc25b8d02f51bd1
Jamie Myland <jamie.myland@gmail.com>
2016-10-14 11:52:45 +0100

    Added new classes in preparation

---
+++ Character.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApplication
+{
+    public class Character
+    {
+        public Character()
+        {
+            
+
+        }
+
+
+    }
+}
---
+++ Inventory.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApplication
+{
+    public class Inventory
+    {
+        public Inventory()
+        {
+            
+
+        }
+
+
+    }
+}
